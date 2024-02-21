7cb82c3e855b466446ea5c3447bd10b62989566e
Radu Ciobanu <radu.ciobanu22@gmail.com>
2024-02-21 23:35:49 +0800

    Fixed menu alignment

--- frontend/src/components/LinxMenu.vue
+++ frontend/src/components/LinxMenu.vue
@@ -44,7 +44,7 @@ const menuItems = [
 
 <template>
   <div>
-    <div class="flex items-center justify-between p-4 lg:invisible">
+    <div class="flex items-center justify-between p-4 lg:hidden">
       <div @click="showSideBar = !showSideBar">
         <MenuButton />
       </div>
@@ -52,7 +52,7 @@ const menuItems = [
       <div></div>
     </div>
     <nav
-      class="sidebar fixed lg:relative lg:flex flex-row text-white items-center justify-between w-56 lg:w-full bg-core-darkest space-x-[20px] h-screen lg:h-auto overflow-y-auto transition-transform -translate-x-full lg:translate-x-0 space-y-[20px]"
+      class="sidebar fixed lg:relative lg:flex flex-row text-white items-center justify-between lg:w-full bg-core-darkest space-x-[20px] h-screen lg:h-auto overflow-y-auto transition-transform -translate-x-full lg:translate-x-0 py-5"
       :class="{ 'absolute -translate-x-0 left-56 ': showSideBar, 'left-0': !showSideBar }"
     >
       <RouterLink :to="'/'">
@@ -60,7 +60,7 @@ const menuItems = [
       </RouterLink>
       <div
         @click="showSideBar = !showSideBar"
-        class="visible lg:invisible sm:h-0 flex items-center space-x-[17px] px-4 pb-[6px]"
+        class="visible lg:invisible sm:h-0 flex items-center pb-[6px]"
       >
         <font-awesome-icon :icon="['fal', 'times']" class="text-lg text-white text-[28px]" />
 
@@ -68,7 +68,7 @@ const menuItems = [
       </div>
 
       <div
-        class="flex flex-col lg:flex-row justify-center pl-[40px] lg:pl-0 lg:space-x-[40px] space-y-[20px] lg:space-y-0 -mb-[3px]"
+        class="flex flex-col lg:flex-row justify-center pl-[40px] lg:pl-0 lg:space-x-[40px] lg:space-y-0 -mb-[3px]"
       >
         <MenuItem
           @click="(selectedMenuItem = item.title), (showSideBar = false)"
