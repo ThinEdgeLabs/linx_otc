1f9b1a38e020f0c0fb401487d2982e6d445cb539
Radu Ciobanu <radu.ciobanu22@gmail.com>
2024-02-22 00:03:47 +0800

    Fixed connect wallet modal position

--- frontend/src/components/WalletSelect.vue
+++ frontend/src/components/WalletSelect.vue
@@ -8,9 +8,9 @@ const loginStore = useLoginStore()
 <template>
   <div
     v-if="loginStore.showModal"
-    class="absolute top-0 left-0 w-full h-full flex justify-center items-center bg-core-darkest bg-opacity-50 backdrop-blur-md dark:text-core-light"
+    class="absolute top-0 left-0 w-full h-screen flex justify-center items-center bg-core-darkest bg-opacity-50 backdrop-blur-md dark:text-core-light"
   >
-    <section class="pt-4 pb-12 px-8 space-y-[20px] rounded-lg bg-core-darkest">
+    <section class="pt-4 pb-12 px-8 py-5 rounded-lg bg-core-darkest">
       <div>
         <h3 class="flex justify-between items-baseline text-2xl font-extrabold leading-10 text-white">
           Connect Wallet
@@ -21,7 +21,7 @@ const loginStore = useLoginStore()
           />
         </h3>
         <div class="text-start text-white opacity-75">
-          <p>Select the Alephium wallet you want to connect to</p>
+          <p>Connect with a supported Alephium wallet</p>
         </div>
       </div>
       <LoginModalVue />
