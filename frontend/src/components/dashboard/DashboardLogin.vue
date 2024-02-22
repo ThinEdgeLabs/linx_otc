ced4485179bf0ef0b4fe3dd825bbf751cac797bf
floordebest <floor@dbestinnovations.com>
2024-02-22 14:09:50 +0100

    add icon dashboard login

--- frontend/src/components/dashboard/DashboardLogin.vue
+++ frontend/src/components/dashboard/DashboardLogin.vue
@@ -4,6 +4,7 @@ import CustomButton from '@/components/CustomButton.vue'
 
 <template>
   <div class="flex flex-col w-full h-full py-[15%] items-center">
+    <font-awesome-icon :icon="['fat', 'plug']" class="text-[38px] text-accent-3 pb-[30px]" />
     <div class="text-[22px] font-extrabold text-core-lightest">Please Connect your Alephium Wallet</div>
     <div class="pb-[30px] text-core-light text-[14px]">Some useless text</div>
     <CustomButton :title="'Connect wallet'" :class="'w-[200px]'" />
