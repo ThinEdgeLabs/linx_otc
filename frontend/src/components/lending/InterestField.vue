36f5a0254aee12c066b59d3929d3252acbcd0a4a
floordebest <floor@dbestinnovations.com>
2024-04-09 14:45:12 +0200

    fix interestfield on create loan

--- frontend/src/components/lending/InterestField.vue
+++ frontend/src/components/lending/InterestField.vue
@@ -14,7 +14,7 @@ function setAmount(amount: number) {
 
 <template>
   <div class="w-full flex flex-col space-y-[10px]">
-    <p class="text-[14px] font-extrabold text-core-light">Amount of interest</p>
+    <p class="text-[14px] font-extrabold text-core-light">You want to receive back</p>
     <div class="flex flex-row w-full rounded-lg bg-white text-center p-[10px] h-[52px]">
       <div v-if="loanOrderStore.order?.loanToken" class="w-full flex flex-row justify-between items-center">
         <div class="w-full flex flex-row items-center space-x-[10px]">
@@ -22,17 +22,22 @@ function setAmount(amount: number) {
             :src="`${loanOrderStore.order?.loanToken.logoUri ?? '/images/tokens/NONE.png'}`"
             class="w-[32px] h-[32px] rounded-full"
           />
+          <p>{{ loanOrderStore.order.loanToken.symbol }}</p>
+          <div class="inline-block w-[0.5px] self-stretch bg-core-light opacity-100"></div>
           <input
             type="number"
             inputmode="decimal"
             class="text-[18px] font-extrabold text-core-darkest remove-arrow outline-none rounded-lg"
-            :value="!loanOrderStore.order?.loanToken ? 'Choose token' : loanOrderStore.order?.interest"
+            :value="
+              !loanOrderStore.order?.loanToken
+                ? 'Choose token'
+                : loanOrderStore.order?.interest + loanOrderStore.order.loanAmount
+            "
             @input="(e) => setAmount(parseFloat((e.target as HTMLInputElement).value))"
           />
         </div>
 
-        <div class="flex flex-row space-x-[10px] items-center">
-          <p>{{ loanOrderStore.order?.loanToken.symbol }}</p>
+        <div class="flex-row space-x-[10px] items-center hidden lg:flex">
           <PercentageButton :interest="1" />
           <PercentageButton :interest="5" />
           <PercentageButton :interest="10" />
