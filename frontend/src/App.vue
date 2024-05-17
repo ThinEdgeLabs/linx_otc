7de21a8b2e76bab21b931c50bf7419903e2153d3
Radu Ciobanu <radu.ciobanu22@gmail.com>
2024-05-17 14:03:23 +0300

    Updated beta warning message

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -31,7 +31,8 @@ onMounted(async () => {
       type: 'warning',
       message: [
         'Please be aware that Linx OTC is still in the BETA phase. This means there may be bugs and errors. Despite our efforts to deliver a safe DApp, contracts could potentially become compromised, which could result in the loss of all your funds.',
-        '\bUse this DApp at your own risk'
+        '\bLinx OTC is currently on TESTNET',
+        '\bLinx Labs is not responsible for any loss of funds, use this DApp at your own risk.'
       ],
       showTerms: true,
       onAcknowledged: acknowledgedTerms,
