e38098557eca47077603d9efe1a73af2c6a1c8ac
Radu Ciobanu <radu.ciobanu22@gmail.com>
2024-05-17 14:00:04 +0300

    Mobile fix

--- frontend/src/components/PageTitle.vue
+++ frontend/src/components/PageTitle.vue
@@ -12,9 +12,9 @@ const props = defineProps<{
     <p class="text-[32px] text-white font-extrabold">{{ props.title }}</p>
     <div class="flex flex-col lg:flex-row justify-between">
       <p class="text-[14px] text-white">{{ props.description }}</p>
-      <div class="flex items-center space-x-[10px] mt-[20px] lg:mt-0">
+      <div class="flex lg:items-center space-x-[10px] mt-[20px] lg:mt-0">
         <font-awesome-icon v-if="props.icon" :icon="props.icon" class="text-[14px] text-warning" />
-        <p v-if="props.descriptionRight" class="text-[14px] text-warning">
+        <p v-if="props.descriptionRight" class="text-[14px] text-warning leading-none">
           {{ props.descriptionRight }}
         </p>
       </div>
