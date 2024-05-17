700a54b1e14fbdfd1b1cdf51107142a0af300802
Radu Ciobanu <radu.ciobanu22@gmail.com>
2024-05-17 13:48:32 +0300

    Small adjustments on FAQ page

--- frontend/src/components/faq/FaqComponent.vue
+++ frontend/src/components/faq/FaqComponent.vue
@@ -13,10 +13,13 @@ const props = defineProps({
   <div class="w-full flex flex-col space-y-[40px] leading-snug">
     <div class="flex flex-col w-full" :class="props.isHomeView ? 'lg:items-center' : ''">
       <p v-if="props.isHomeView" class="text-[14px] text-accent-1">FAQ</p>
-      <p class="text-[28px] lg:text-[40px] font-extrabold text-core-lightest">Frequently Asked Questions</p>
-      <p class="text-[16px] lg:text-[18px] text-core-light" :class="props.isHomeView ? 'lg:w-[35%]' : 'w-full'">
-        Got a question? Your answer might be here. If you can't find what you're looking for, join our Discord and ask
-        us directly.
+      <p class="text-[28px] lg:text-[34px] font-extrabold text-core-lightest">Frequently Asked Questions</p>
+      <p class="text-[16px] lg:text-[16px] text-core-light" :class="props.isHomeView ? 'lg:w-[35%]' : 'w-full'">
+        Got a question? Your answer might be here. If you can't find what you're looking for,
+        <a class="font-extrabold text-accent-3 underline" href="https://discord.gg/g434BaWTFb" _target="blank"
+          >join our Discord</a
+        >
+        and ask us directly.
       </p>
     </div>
     <div class="flex flex-col w-full space-y-[20px]">
