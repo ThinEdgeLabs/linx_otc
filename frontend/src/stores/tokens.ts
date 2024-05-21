d80c8b5122fa8dd253302b6d988aa3dfb7aa2e2a
Floor de Best <debestfloor@gmail.com>
2024-05-21 12:13:28 +0200

    handle inittokens in try catch

--- frontend/src/stores/tokens.ts
+++ frontend/src/stores/tokens.ts
@@ -8,35 +8,40 @@ export const useTokenStore = defineStore('tokens', () => {
   const tokens = ref<Array<Token> | undefined>(undefined)
 
   async function initTokens(network: string) {
-    const res = await fetch(`https://raw.githubusercontent.com/alephium/token-list/master/tokens/${network}.json`)
-    if (res.ok) {
-        const tokensList = await res.json()
-        for (const token of tokensList.tokens) {
+    try {
+        const res = await fetch(`https://raw.githubusercontent.com/alephium/token-list/master/tokens/${network}.json`)
+        if (res.ok) {
+            const tokensList = await res.json()
+            for (const token of tokensList.tokens) {
+                if (!tokens.value) {
+                    tokens.value = []
+                }
+                tokens.value.push({
+                    symbol: token.symbol,
+                    name: token.name,
+                    contractId: token.id,
+                    decimals: token.decimals,
+                    logoUri: token.logoURI
+                })
+            }
+        }
+        // Add custom token not on the github repository
+        if (network === 'testnet') {
             if (!tokens.value) {
                 tokens.value = []
             }
-            tokens.value.push({
-                symbol: token.symbol,
-                name: token.name,
-                contractId: token.id,
-                decimals: token.decimals,
-                logoUri: token.logoURI
-            })
+            tokens.value.push({ 
+                symbol: 'TST-1',
+                name: 'TestToken-1',
+                contractId: '2e18c64cc2d8aa88b7f0d9de7254f4f5283ec4ce1e94434b9a97c03266406901',
+                decimals: 18,
+                logoUri: '/images/tokens/nologo.png'
+          })
         }
+    } catch (error) {
+        console.log('Unable to get tokens list', error)
     }
-    // Add custom token not on the github repository
-    if (network === 'testnet') {
-        if (!tokens.value) {
-            tokens.value = []
-        }
-        tokens.value.push({ 
-            symbol: 'TST-1',
-            name: 'TestToken-1',
-            contractId: '2e18c64cc2d8aa88b7f0d9de7254f4f5283ec4ce1e94434b9a97c03266406901',
-            decimals: 18,
-            logoUri: '/images/tokens/nologo.png'
-      })
-    }
+
   }
 
   async function getTokens(network: string) {
