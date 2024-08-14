b86da5c5e1586e3ed6fb83e4130e6bff52745e98
Radu Ciobanu <radu.ciobanu22@gmail.com>
2024-08-14 13:46:50 +0800

    Calculate LTV

--- test/unit/LendingMarketplaceUtils.test.ts
+++ test/unit/LendingMarketplaceUtils.test.ts
@@ -1,5 +1,11 @@
 import { web3 } from '@alephium/web3'
 import { TestLendingMarketplaceUtils } from '../../artifacts/ts'
+import { TokenPrice } from '../../artifacts/ts/types'
+import { expandTo18Decimals } from '../../shared/utils'
+
+////////////////////////////////////
+// -------- Test helpers -------- //
+////////////////////////////////////
 
 function normalizeAmount(amount: bigint, decimals: bigint) {
   return TestLendingMarketplaceUtils.tests.normalizeAmount({
@@ -12,6 +18,23 @@ function blockTimeStampInSeconds(blockTimeStamp: number) {
     blockTimeStamp: blockTimeStamp
   })
 }
+
+async function calculateLTV(
+  collateralTokenAmount: bigint,
+  collateralTokenPrice: TokenPrice,
+  borrowedTokenAmount: bigint,
+  borrowedTokenPrice: TokenPrice
+) {
+  return (
+    await TestLendingMarketplaceUtils.tests.calculateLTV({
+      testArgs: { collateralTokenAmount, collateralTokenPrice, borrowedTokenAmount, borrowedTokenPrice }
+    })
+  ).returns
+}
+
+////////////////////////////////////
+// ----------- Tests ------------ //
+////////////////////////////////////
 
 describe('LendingMarketplaceUtils', () => {
   beforeAll(async () => {
@@ -44,6 +67,30 @@ describe('LendingMarketplaceUtils', () => {
       const timestamp = Date.now()
       const testResult = await blockTimeStampInSeconds(timestamp)
       expect(testResult.returns).toEqual(BigInt(Math.floor(timestamp / 1000)))
+    })
+  })
+
+  describe('calculateLTV', () => {
+    it('should return the LTV if collateral value >= borrowed value', async () => {
+      const borrowedAmount = expandTo18Decimals(1000n)
+      const borrowedTokenPrice = { price: 100000000n, timestamp: BigInt(Date.now()), decimals: 8n }
+      const collateralAmount = expandTo18Decimals(2000n)
+      const collateralTokenPrice = { price: 150000000n, timestamp: BigInt(Date.now()), decimals: 8n }
+
+      const ltv = await calculateLTV(collateralAmount, collateralTokenPrice, borrowedAmount, borrowedTokenPrice)
+
+      expect(ltv).toEqual(expandTo18Decimals(3))
+    })
+
+    it('should return the LTV if collateral value < borrowed value', async () => {
+      const borrowedAmount = expandTo18Decimals(1000n)
+      const borrowedTokenPrice = { price: 100000000n, timestamp: BigInt(Date.now()), decimals: 8n }
+      const collateralAmount = expandTo18Decimals(500n)
+      const collateralTokenPrice = { price: 150000000n, timestamp: BigInt(Date.now()), decimals: 8n }
+
+      const ltv = await calculateLTV(collateralAmount, collateralTokenPrice, borrowedAmount, borrowedTokenPrice)
+
+      expect(ltv).toEqual(750000000000000000n)
     })
   })
 })
